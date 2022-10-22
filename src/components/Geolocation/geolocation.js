387f729542d5e9c93de44f2d112ca1a68b1f8c97
Devang2304 <devangvartak2304@gmail.com>
2022-10-23 03:31:32 +0530

    created Geolocation function which tracks the current location and display to us

---
+++ src/components/Geolocation/geolocation.js
@@ -0,0 +1,38 @@
+import React,{useEffect, useState} from "react";
+const Geolocation=()=>{
+    const [location,setLocation]=useState({
+        loaded:false,
+        coordinates:{lat:"",lng:""}
+    });
+
+    const onSucess=location=>{
+        setLocation({
+            loaded:true,
+            coordinates:{
+                lat:location.coords.latitude,
+                lng:location.coords.longitude,
+            },
+        });
+    };
+
+    const onError=error=>{
+        setLocation({
+            loaded:true,
+            error:{
+                message:error.message,
+            },
+        });
+    }
+
+    useEffect(()=>{
+        if( !("geolocation" in navigator)){
+           onError({
+            code:0,
+            message:"Geolocation is not supported",
+            });
+        }
+    navigator.geolocation.getCurrentPosition(onSucess,onError); 
+    },[]);
+    return location;
+};
+export default Geolocation;
