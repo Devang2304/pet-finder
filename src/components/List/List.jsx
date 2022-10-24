47b51ce1b66e7e5c097b2641411979bb998f01e9
Devang2304 <devangvartak2304@gmail.com>
2022-10-24 13:57:52 +0530

    UploadImage function added but issues faced

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Geolocation from "../Geolocation/geolocation";
+// import Geolocation from "../Geolocation/geolocation";
 import { CircularProgress,Grid,Typography,InputLabel,MenuItem,FormControl,Select,Button } from "@material-ui/core";
-
+import UploadImage from "../ImageUpload/UploadImage";
 import useStyles from "./styles";
 import { render } from "@testing-library/react";
 
@@ -10,16 +10,16 @@ import { render } from "@testing-library/react";
 
 const List =()=>{
     const classes = useStyles();
-    const location=Geolocation();
+    // const location=Geolocation();
     return (
         
         <div className={classes.container}>
             <Typography variant="h5">If you know any stress dog if give your current location & dog's photo</Typography>
-            <Button className={classes.Button} variant="contained">Upload Image</Button>
+            <Button className={classes.Button} onClick={<UploadImage/>} variant="contained">Upload Image</Button>
             <Button className={classes.Button} variant="contained">Current Location</Button>
-            {
+            {/* {
                 location.loaded ? JSON.stringify(location) : "location is not available yet"
-            }
+            } */}
             <Typography variant="h4">Stress dogs around you</Typography>
         </div>
     );
